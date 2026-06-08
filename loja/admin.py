67ebd2421b5a99bebd0857b21b5678675ebda4bc
BRYAN OLIVEIRA DE CARVALHO <bryan.carvalho@aluno.senac.rio>
2026-06-08 20:45:47 -0300

    08/06/2026

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Produto
 
 # Register your models here.
+@admin.register(Produto)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao', 'preco')  
+    
+    list_filter = ('disponivel',) 
+
+    search_fields = ('nome',)
--- loja/views.py
+++ loja/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def inicio(request):
@@ -8,7 +9,11 @@ def sobre(request):
     return render(request, 'app/sobre.html')
 
 def catalogo(request):
-    return render(request, 'app/catalogo.html')
+    lista_produtos = Produto.objects.filter(disponivel = True)
+    contexto = {
+        'produtos': lista_produtos
+    }
+    return render(request, 'app/catalogo.html', contexto)
 
 def contato(request):
     return render(request, 'app/contato.html')
